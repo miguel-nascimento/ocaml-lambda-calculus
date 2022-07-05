08358718943af724557b456aa416be1c8bb1f635
Miguel Nascimento <miguelgomes13@live.com>
2022-07-05 02:18:16 -0300

    chore: expr pp

--- systemf/lib/expr.ml
+++ systemf/lib/expr.ml
@@ -8,3 +8,19 @@ type expr =
   | L_int of int
   | T_lamb of { typ_param : string; body : expr }
   | T_app of { expr : expr; typ : typ }
+
+let rec expr_pp expr =
+  match expr with
+  | Var name -> name
+  | Lamb { param; param_typ = T_var _ as param_typ; body }
+  | Lamb { param; param_typ = T_int as param_typ; body } ->
+      "λ" ^ param ^ ":" ^ typ_pp param_typ ^ ". " ^ expr_pp body
+  | Lamb { param; param_typ; body } ->
+      "λ" ^ param ^ ":(" ^ typ_pp param_typ ^ "). " ^ expr_pp body
+  | App { f = App _ as f; arg } -> "(" ^ expr_pp f ^ " " ^ expr_pp arg ^ ")"
+  | App { f; arg } -> "(" ^ expr_pp f ^ " " ^ expr_pp arg ^ ")"
+  | L_int i -> string_of_int i
+  | T_lamb { typ_param; body } -> "Λ" ^ typ_param ^ ". " ^ expr_pp body
+  | T_app { expr; typ = T_var _ as typ } | T_app { expr; typ = T_int as typ } ->
+      expr_pp expr ^ " @" ^ typ_pp typ
+  | T_app { expr; typ } -> expr_pp expr ^ " @(" ^ typ_pp typ ^ ")"
